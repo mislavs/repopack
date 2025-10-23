9017d80cc14bfc0886ca8882022c96406e2a9532
Mislav Starzik <mislav.starzik@thoughtcap.com>
2025-10-23 11:03:05 +0200

    Add file_path argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome to repopack!");
+
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem passing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Packing repository located in: {}", config.file_path);
+}
+
+struct Config {
+    file_path: String
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 2 {
+            return Err("Not enough arguments");
+        }
+
+        let file_path = args[1].clone();
+
+        Ok(Config { file_path })
+    }
 }
